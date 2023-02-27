e720bab3346ef24599d0038f55072494d6ec054b
RobertWallström <robert.wallstrom@varderingsdata.se>
2023-02-27 17:30:33 +0100

    Add configuration classes

---
+++ src/Quartz.Spi.MongoDbJobStore/Configuration/QuartzMongoDbExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Quartz;
+
+namespace Quartz.Spi.MongoDbJobStore.Configuration
+{
+    public static class QuartzMongoDbExtensions
+    {
+        public static void UseMongoDb(this SchedulerBuilder.PersistentStoreOptions options, Action<QuartzMongoDbOptions> configure)
+        {
+            configure?.Invoke(new QuartzMongoDbOptions(options));
+        }
+
+        public static void UseMongoDbPersistantStorage(
+            this IServiceCollectionQuartzConfigurator configurator, Action<QuartzMongoDbOptions> configure)
+        {
+
+            configurator.UsePersistentStore<MongoDbJobStore>(options =>
+            {
+                options.UseMongoDb(configure);
+            });
+        }
+    }
+}
---
+++ src/Quartz.Spi.MongoDbJobStore/Configuration/QuartzMongoDbOptions.cs
@@ -0,0 +1,36 @@
+﻿using Quartz.Impl;
+
+namespace Quartz.Spi.MongoDbJobStore.Configuration
+{
+    public class QuartzMongoDbOptions : SchedulerBuilder.StoreOptions
+    {
+
+        public QuartzMongoDbOptions(PropertiesHolder? parent = null) : base(parent ?? SchedulerBuilder.Create())
+        {
+        }
+
+        public string? InstanceName
+        {
+            set => SetProperty(StdSchedulerFactory.PropertySchedulerInstanceName, value);
+            get => Properties.Get(StdSchedulerFactory.PropertySchedulerInstanceName);
+        }
+
+        public string? InstanceNameId
+        {
+            set => SetProperty(StdSchedulerFactory.PropertySchedulerInstanceId, value);
+            get => Properties.Get(StdSchedulerFactory.PropertySchedulerInstanceId);
+        }
+
+        public string? ConnectionString
+        {
+            set => SetProperty($"{StdSchedulerFactory.PropertyJobStorePrefix}.{StdSchedulerFactory.PropertyDataSourceConnectionString}", value);
+            get => Properties.Get($"{StdSchedulerFactory.PropertyJobStorePrefix}.{StdSchedulerFactory.PropertyDataSourceConnectionString}");
+        }
+
+        public string? CollectionPrefix
+        {
+            set => SetProperty($"{StdSchedulerFactory.PropertyJobStorePrefix}.collectionPrefix", value);
+            get => Properties.Get($"{StdSchedulerFactory.PropertyJobStorePrefix}.collectionPrefix");
+        }
+    }
+}
